b35619a5ed385c4bf87b5f026ea711b2434e8761
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-04-19 17:57:15 -0300

    "I am big. It's the pictures that got small". :clapper:
    In Sunset Boulevard
    
    --
    Configura angular-spotify no app.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,30 @@
 
-var app = angular.module("app", []);
+var app = angular.module('myApp', ['spotify', 'ui.router']);
 
+app.config(function (SpotifyProvider) {
+    SpotifyProvider.setClientId('38c15beb323a4c5498909d62bd796998');
+    SpotifyProvider.setRedirectUri('http://localhost:8888/#/callback');
+    SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    var home = {
+        name: "home",
+        url: '/home',
+        templateUrl: 'app/views/home.html',
+        controller: 'HomeController as homeCtrl'
+    }
+
+    var callback = {
+        name: "callback",
+        url: '/callback',
+        templateUrl: 'app/views/callback.html'
+    }
+
+    $stateProvider.state(callback);
+    $stateProvider.state(home);
+
+});
--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,7 +1,16 @@
 
-app.controller('HomeController', function($scope) {
+app.controller('HomeController', function ($scope, Spotify) {
 
     var self = this;
+
+    self.login = function () {
+        Spotify.login().then(function (data) {
+            console.log(data);
+            alert("You are now logged in");
+        }).catch(function(error) {
+            console.log('didn\'t log in');
+        });
+    };
 
     (function main() {
 
